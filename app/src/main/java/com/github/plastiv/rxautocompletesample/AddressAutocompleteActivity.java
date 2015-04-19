3273bec58bbab48afb178294393c104b12755470
Sergii Pechenizkyi <spechenizkyi@brightgrove.com>
2015-04-19 13:38:15 +0300

    Added searchview.

--- app/src/main/java/com/github/plastiv/rxautocompletesample/AddressAutocompleteActivity.java
+++ app/src/main/java/com/github/plastiv/rxautocompletesample/AddressAutocompleteActivity.java
@@ -4,6 +4,7 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.SearchView;
 
 public class AddressAutocompleteActivity extends Activity {
 
@@ -17,21 +18,13 @@ public class AddressAutocompleteActivity extends Activity {
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_address_autocomplete, menu);
+
+        MenuItem item = menu.findItem(R.id.action_search);
+        item.expandActionView(); // get focus
+
+        SearchView searchView = (SearchView) item.getActionView();
+
+        // TODO: set query listener
         return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
     }
 }
